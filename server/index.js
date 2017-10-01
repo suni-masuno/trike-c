9e18649fb3817805cfcd902c5299fe008e510901
Lauren Hilton <lauren.hilton@daugherty.com>
2017-09-30 22:59:31 -0500

    sorry...

--- server/index.js
+++ server/index.js
@@ -9,12 +9,36 @@ export default new Server()
 import Express from 'express';
 import './common/env';
 import * as bodyParser from 'body-parser';
+import fetch from 'node-fetch';
 
 const app = new Express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('*', (request, response, next) => {
-  console.log(request.body);
+app.post('/api/v1/ruleSet', (request, response, next) => {
+  const body = JSON.parse(Object.keys(request.body)[0]);
+  console.log(body);
+  fetch('http://localhost:5984/rulesets',
+    {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body) },
+  );
+  response.sendStatus(200);
+});
+app.get('/api/v1/ruleSet', (request, response, next) => {
+  fetch('http://localhost:5984/rulesets/_all_docs?include_docs=true&decending=true&limit=1',
+    {
+      method: 'get',
+      headers: {
+        'Content-type': 'application/json',
+      }},
+  ).then(couchResponse => {
+    return couchResponse.json();
+  }).then(json => {
+    response.send(json.rows[0].doc);
+  });
 });
 
 app.listen(process.env.PORT);
